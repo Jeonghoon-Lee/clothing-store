be19e6d9161ebdfb3f238f03085191abbd1f26ce
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-07-16 10:16:16 -0400

    fixed a bug in signinWithGoogle button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -63,7 +63,11 @@ class SignIn extends Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignin>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignin
+            >
               Sign in with Google
             </CustomButton>
           </div>
